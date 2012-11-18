d2478ca6c639a323178dc0f8870d41c21a63daca
Thomas Crescenzi <trescenzi@gmail.com>
2012-11-18 12:07:17 +0900

    event crontroller and view in progress

---
+++ app/controllers/EventController.scala
@@ -0,0 +1,11 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import models._
+import play.api.data._
+import play.api.data.Forms._
+
+object EventController extends Controller {
+
+}
